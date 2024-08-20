17ad15b6099bd935ba2c9fd9aabf7051342fd4ea
rstefko <roman.stefko@eway-crm.com>
2024-08-20 11:36:32 +0200

    Initial version

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"ExecutableTrigger failed with exception: {ex.Message}");
+            }
+        }
+    }
+}
